f14ba1a8fc883d96b4efe55860854e2a9c800296
Ahsan <ak31111111@gmail.com>
2019-02-27 16:04:34 +0500

    commit from lab

--- ps0/src/rules/RulesOf6005.java
+++ ps0/src/rules/RulesOf6005.java
@@ -35,7 +35,7 @@ public class RulesOf6005 {
     public static boolean mayUseCodeInAssignment(boolean writtenByYourself,
             boolean availableToOthers, boolean writtenAsCourseWork,
             boolean citingYourSource, boolean implementationRequired) {
-        
+        //changes
         // TODO: Fill in this method, then remove the exception
         
         throw new RuntimeException("implement me!");
@@ -51,5 +51,6 @@ public class RulesOf6005 {
     public static void main(String[] args) {
         System.out.println("You may certainly use code you wrote yourself: " +
             RulesOf6005.mayUseCodeInAssignment(true, false, true, true, true));
+        System.out.println("hello");
     }
 }
