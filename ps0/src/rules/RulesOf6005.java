47cdcd608c1285b45b2160136f1cbe17808060b4
hajiraihsan <hajiraihsan8@gmail.com>
2019-02-28 18:46:18 +0500

    problem set 0 part 1

--- ps0/src/rules/RulesOf6005.java
+++ ps0/src/rules/RulesOf6005.java
@@ -40,6 +40,7 @@ public class RulesOf6005 {
     	
         if (writtenByYourself==true && availableToOthers==false && writtenAsCourseWork==true && citingYourSource==true && implementationRequired==true)
          {
+        	
         	oneCanUseTheCode=true;
          	
          	
